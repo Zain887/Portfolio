75e2bd6bbf64626c1e5397adbf1c29a725e29276
muhammad zain <zainansari887@gmail.com>
2023-11-25 22:22:11 +0500

    fix issue unusedfiled

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -7,7 +7,9 @@ interface Props {
 const Home: React.FC<Props> = (props) => {
     return (
         <div>
+            {/*  */}
             <p className='text-8xl font-extrabold font-sans text-[#F9DF7B] my-20 text-center'>SomeThingAwsomeComingSoon!</p>
+        
         </div>
     );
 };
